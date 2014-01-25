4d7161b101ec713326161d907061144a553c0615
hezt95 <hezt95@gmail.com>
2014-01-26 00:36:28 +0800

    .Remove the global name and change something

--- proxy-v1.py
+++ proxy-v1.py
@@ -5,79 +5,53 @@ import sys
 import time
 import re
 
-
-
-def cli_proxy_socket(host_proxy,port_proxy):
-	try:
-		sock = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
-		sock.setsockopt(socket.SOL_SOCKET,socket.SO_REUSEADDR,1)	
-	except socket.error, e:
-		print 'socket client to proxy false:%s'%e
-	print 'Socket client to proxy created'
-	try:
-		sock.bind((host_proxy, port_proxy))
-	except socket.error , e:
-		print 'Bind client to proxy failed:%s'%e
-		sys.exit()
-	print 'Socket client to proxy bind complete'
-	sock.listen(5)
-	print 'Socket client to proxy now listening'
-
-	global addr
-	conn,addr = sock.accept()
-	return conn
-#keep talking
-def cli_proxy_data(conn,addr):
+def cli_proxy_recvdata(conn,addr):
 	try:
 		print 'Connected with ' + addr[0] + ':' + str(addr[1])		
-		recvdata_cli = conn.recv(81960)
-		######
-		#recvdatas += recvdata
-		if (len(recvdata_cli)>0):	
-			proxy_ser(recvdata_cli)		
-			
-		else:
-			print 'Socket client to proxy trans complete'
+		data = conn.recv(8196)
+		return data
 	except socket.error,msgRece:
 		print 'something wrong in receieve(client to proxy)',msgRece		
 		conn.close()
 		sock.close()
-	return recvdata_cli
-def proxy_ser(recvdata_cli):
-	recvdatalist = re.split(r'\r\n', recvdata_cli) 
-	Reqline = str(recvdatalist[0])
-	try:
-		sock_ser = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
-		sock_ser.setsockopt(socket.SOL_SOCKET,socket.SO_REUSEADDR,1)
-	except socket.error, e:
-		print 'socket to server false:%s'%e
 
-	print 'socket to server'
+def proxy_ser_socket(data):
+	if (len(data)>0):
+		#prosess the recvdata	
+		datalist = re.split(r'\r\n', data) 
+		Reqline = str(datalist[0])
+		host = str(datalist[1])[6:]
+		try:
+			sock = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
+			sock.setsockopt(socket.SOL_SOCKET,socket.SO_REUSEADDR,1)
+
+		except socket.error, e:
+			print 'Socket to server false:%s'%e			
+		print 'Creat socket with server'
+		print 'INFO:',Reqline
+		
+		try:
+			sock.connect((host,80))
+		except socket.error, e:
+			print 'Connect to server false:%s'%e
+		print 'Connect to server'
+		try:
+			print'Sending to server'
+			sock.send(data)
+		except socket.error,e:
+			print 'Send to server false:%s'%e
+			sock.close()
+		return sock
+		
+	else:
+		print 'Proxy cannot get data from client'
+
 	
-	print 'INFO:',Reqline
-	host_ser = str(recvdatalist[1])[6:]
-	print host_ser
-	try:
-		sock_ser.connect((host_ser,80))
-	except socket.error, e:
-		print 'connect to server false:%s'%e
 	
-	print 'connect to server'
-	try:
-		print'sending to server'
-		senddata_ser = recvdata_cli
-		sock_ser.send(senddata_ser)
-	except socket.error,e:
-		print 'send to server false:%s'%e
-		sock_ser.close()
-
-	recv_timeout(sock_ser,timeout=2)
-
-
-
-def recv_timeout(the_socket,timeout=2):
+def recv_data_ser_send(sock_ser,sock_conn_cli,timeout=2):
+	#it can make all data into one variable:final_total_data(str)
 	#make socket non blocking
-	the_socket.setblocking(0)
+	sock_ser.setblocking(0)
 	#total data partwise in an array
 	total_data=[]
 	data=''
@@ -92,29 +66,46 @@ def recv_timeout(the_socket,timeout=2):
 			break
 		#recv something
 		try:
-			data = the_socket.recv(8192)
-			if data:
-				total_data.append(data)
-				#change the beginning time for measurement
-				begin = time.time()
-			else:
-				#sleep for sometime to indicate a gap
-				time.sleep(0.1)
+			data = sock_ser.recv(1024)
+			total_data.append(data)
+			#change the beginning time for measurement
+			begin = time.time()
+			#sleep for sometime to indicate a gap
+			time.sleep(0.1)
 		except:
 			pass
 	#join all parts to make final 'string'
-	global final_total_data
 	final_total_data = ''.join(total_data)
+	return final_total_data
 
-	
-def proxy_cli_data(conn_cli_pro,final_total_data):
-	print final_total_data
-	conn.send(final_total_data)
+#调用函数
+def main(host_proxy,port_proxy):
+	#while 1:
+		#client to proxy socket
+		try:
+			sock = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
+			sock.setsockopt(socket.SOL_SOCKET,socket.SO_REUSEADDR,1)	
+		except socket.error, e:
+			print 'socket client to proxy false:%s'%e
+		print 'Socket client to proxy created'
+		try:
+			sock.bind((host_proxy, port_proxy))
+		except socket.error , e:
+			print 'Bind client to proxy failed:%s'%e
+			sys.exit()
+		print 'Socket client to proxy bind complete'
+		sock.listen(5)
+		print 'Socket client to proxy now listening'
+		conn,addr = sock.accept()#connection
+		
+		recvdata_cli = cli_proxy_recvdata(conn,addr)#recieve data from client include the request line 
+		sock_ser = proxy_ser_socket(recvdata_cli)
+		final_total_data = recv_data_ser_send(sock_ser,conn,timeout=2)
+		conn.send(final_total_data)
+		conn.close()
+		sock.close()
 
-
-conn = cli_proxy_socket('',55555)
-cli_proxy_data(conn,addr)
-proxy_cli_data(cli_proxy_socket,final_total_data)
+main('',55555)
 
 
 
